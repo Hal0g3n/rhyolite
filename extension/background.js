b665462e318f9b15bdddb66c7796ac770c5be7ae
Haloen <72333894+Haloen@users.noreply.github.com>
2022-11-11 20:50:00 +0800

    Fixed the pinning problem

--- extension/background.js
+++ extension/background.js
@@ -1,15 +1,4 @@
-function jumpToHome() {
-    chrome.tabs.query({ currentWindow: true, index: 0 }, (tab) => {
-        chrome.tabs.update(tab[0].id, { active: true });
-    });
-}
-
-function createTab(id) {
-    chrome.tabs.create({ url: "./index.html", windowId: id, active: false, pinned: true }, (tab) => {
-        chrome.tabs.move(tab.id, { index: 0 });
-    })
-}
-
+// Shortcut Command Listener
 chrome.commands.onCommand.addListener(function (command) {
     switch (command) {
         case 'Jump to Home':
@@ -20,21 +9,42 @@ chrome.commands.onCommand.addListener(function (command) {
     }
 });
 
-// when a new window is created, open index.html
+// Shortcut function for jumping to first page
+function jumpToHome() {
+    chrome.tabs.query({ currentWindow: true, index: 0 }, (tab) => {
+        chrome.tabs.update(tab[0].id, { active: true });
+    });
+}
+
+// Creates the Home Tab (pinned) in the given window
+function createTab(id) {
+    chrome.tabs.create({ url: "./index.html", windowId: id, active: false, pinned: true }, (tab) => {
+        chrome.tabs.move(tab.id, { index: 0 });
+    })
+}
+
+
+// when a new window is created, create tab to go with it
 chrome.windows.onCreated.addListener((window) => {
     createTab(window.id)
 });
 
-
-// Check on startup if any windows lack the page
+// Create tab for all windows
 chrome.windows.getAll((windows) => {
     for (window of windows) {
-        createTab(window.id)
+
+        // Delete existing (but dead) extension tabs
         chrome.tabs.query({windowId: window.id}, (tabs) => {
-            console.log("Window:", window.id, tabs)
-            if (tabs.length == 0) return;
-            chrome.tabs.remove(tabs[0].id);
+            for (tab of tabs) {
+                if (!tab.url.includes(chrome.runtime.id)) continue;
+
+                // Unpin and Remove the dead tab
+                chrome.tabs.update(tab.id, {pinned: false}, (tab) => chrome.tabs.remove(tab.id));
+            }
         })
+
+        // Create the tab to go with it
+        createTab(window.id)
     }
 });
 
