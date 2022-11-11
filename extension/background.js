0bdb5e8f3d336d1b0841d9023304e377b391197c
Haloen <72333894+Haloen@users.noreply.github.com>
2022-11-11 20:36:10 +0800

    Pinning + Window startup thingy

--- extension/background.js
+++ extension/background.js
@@ -4,6 +4,12 @@ function jumpToHome() {
     });
 }
 
+function createTab(id) {
+    chrome.tabs.create({ url: "./index.html", windowId: id, active: false, pinned: true }, (tab) => {
+        chrome.tabs.move(tab.id, { index: 0 });
+    })
+}
+
 chrome.commands.onCommand.addListener(function (command) {
     switch (command) {
         case 'Jump to Home':
@@ -15,15 +21,28 @@ chrome.commands.onCommand.addListener(function (command) {
 });
 
 // when a new window is created, open index.html
-chrome.windows.onCreated.addListener(function() {
-    chrome.tabs.create({url: "./index.html" });
+chrome.windows.onCreated.addListener((window) => {
+    createTab(window.id)
 });
 
-chrome.contextMenus.removeAll();
-chrome.contextMenus.create({
-      title: "first",
-      contexts: ["browser_action"],
-      onclick: function() {
-        alert('first');
-      }
+
+// Check on startup if any windows lack the page
+chrome.windows.getAll((windows) => {
+    for (window of windows) {
+        createTab(window.id)
+        chrome.tabs.query({windowId: window.id}, (tabs) => {
+            console.log("Window:", window.id, tabs)
+            if (tabs.length == 0) return;
+            chrome.tabs.remove(tabs[0].id);
+        })
+    }
 });
+
+// chrome.contextMenus.removeAll();
+// chrome.contextMenus.create({
+//       title: "first",
+//       contexts: ["browser_action"],
+//       onclick: function() {
+//         alert('first');
+//       }
+// });
